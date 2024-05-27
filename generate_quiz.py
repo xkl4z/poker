0b948b1bc090c65937a963c035a77ab6af352934
xkl4z <gsdh.04@gmail.com>
2024-05-27 16:19:58 -0400

    Add quiz for 6-ring RFI

---
+++ generate_quiz.py
@@ -0,0 +1,30 @@
+from util.deck import Deck
+from util.table import Table
+
+deck = Deck()
+deck.new_round()
+deck.shuffle()
+
+table = Table()
+
+
+def main(args):
+
+    with open(args.output_filepath, "w") as f:
+
+        while len(deck.remaining) > 0:
+            cards = deck.deal() + deck.deal()
+            table.actions = table.generate_actions(mode="RFI", length=6, hero_cards=cards)
+            s = table.to_string()
+            f.write(s + "\n====\n\n")
+
+
+if __name__ == "__main__":
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output_filepath")
+    args = parser.parse_args()
+
+    main(args)
